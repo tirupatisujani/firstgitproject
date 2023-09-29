0919af739dc02a2afcb38bbeee633001c8819276
Sujani <tirupatisujani@example.com>
2023-09-29 19:01:35 +0530

    added new class

--- AutomationTesting/src/test/java/Maven_Project1/Chromeview23.java
+++ AutomationTesting/src/test/java/Maven_Project1/Chromeview23.java
@@ -3,10 +3,9 @@ package Maven_Project1;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Chromeview {
+public class Chromeview23 {
 
 	public static void main(String[] args) {
-		WebDriver nn=new ChromeDriver();
 		WebDriver nn=new ChromeDriver();
         nn.get("https://www.irctc.co.in/nget/train-search");
 	}
